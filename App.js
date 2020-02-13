e50bfa0c7e09ecfd9426c8a57ccdbba67626bd97
ji <jkim0107@gmail.com>
2020-02-13 10:24:08 -0800

    rendering test component

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Text, AppRegistry } from 'react-native'
+
+const App = () => {
+  return <Text>Hi there</Text>
+}
+
+AppRegistry.registerComponent('cars', () => App)
+export default App
