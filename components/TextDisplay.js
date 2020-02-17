60cbde6c33ec0668cf948680d36a3c1fd3cfa48f
ji <jkim0107@gmail.com>
2020-02-16 18:19:07 -0800

    updated textdisplay styling

--- components/TextDisplay.js
+++ components/TextDisplay.js
@@ -3,15 +3,24 @@ import { View, Text, StyleSheet } from 'react-native'
 
 const TextDisplay = props => {
   return (
-    <View>
-      <Text>{props.label}</Text>
-      <Text>{props.text}</Text>
+    <View style={styles.viewStyle}>
+      <Text style={styles.labelStyle}>{props.label}</Text>
+      <Text style={styles.textStye}>{props.text}</Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-
+  viewStyle: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start'
+  },
+  labelStyle: {
+    width: 100,
+    fontWeight: 'bold'
+  },
+  textStyle: {
+  }
 })
 
 export default TextDisplay
